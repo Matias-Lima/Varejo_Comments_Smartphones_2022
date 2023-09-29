b183ab14c00cb965b15a0863914c4321a39ca715
Limamat1as <limamatias2002@gmail.com>
2023-09-29 12:36:20 -0300

    update

---
+++ Varejo_comments_smartphones_2022.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+
+# Title
+st.subheader("Varejo - Smartphones - 2022")
+st.title('Comments Per Day Visualization')
+
+# Upload a CSV file
+@st.cache_data
+def carregar_dados():
+    tabela = pd.read_excel("Comments_counts_Smartphone_2022.xlsx")
+    return tabela
+
+with st.container():
+    st.write("---")
+    final_dataframe = carregar_dados()
+    st.area_chart(final_dataframe,x="Date", y="Data")
+
+
